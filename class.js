021083b52ecf534c862f98028e33cce7881f6f10
jungjieun <je_eun2735@naver.com>
2023-03-28 20:06:01 +0900

    public,private

--- class.js
+++ class.js
@@ -18,6 +18,17 @@ set age(value){
   this._age = value <0 ? 0: value; //좀 더 젠틀하게 사용하기 ->value<0 이면 0 아니면 value
   }
 }
-
+// public , private
 const user1 =new user ('jieun','-1','20');
 console.log(user1);
+
+class Experiment{
+  publicfield = 2;
+  //외부에서 접근 가능
+  #privateField = 0;
+  //class 내부에서만 접근가능, 값 변경
+}
+
+const Experiment = new Experiment();
+console.log(experiment.publicfield);
+console.log(experiment.privateField);
