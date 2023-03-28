377ca008ab34e3ee494ee4561019e7b5157d8e5f
jungjieun <je_eun2735@naver.com>
2023-03-28 19:59:58 +0900

    new user

--- class.js
+++ class.js
@@ -1,4 +1,4 @@
-class usrs {
+class user {
   constructor(name,age,number){
     this.name = name;
     this.age = age;
@@ -7,14 +7,17 @@ class usrs {
 
 
 get age(){
-  return this._age
+  return this._age;
   //age와 중복되면 안되므로 _age라고 사용
+  //이제 age 의 값을 가져오기전에 get->set 거쳐서 올라간다.
 }
 set age(value){
   // if (value<0){
   //   throw Error('age can not be negative');
   // } 나이는 -값이 될 수 없으므로 value 값을 setter로 제한
-  this._age = value<0 ? 0: value; //좀 더 젠틀하게 사용하기 ->value<0 이면 0 아니면 value
-}
+  this._age = value <0 ? 0: value; //좀 더 젠틀하게 사용하기 ->value<0 이면 0 아니면 value
+  }
 }
 
+const user1 =new user ('jieun','-1','20');
+console.log(user1);
