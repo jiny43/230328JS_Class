8eb54e5b3e9513018d2868aeb10f1cd2adaeba87
jungjieun <je_eun2735@naver.com>
2023-03-28 20:51:30 +0900

    다양성

--- inheritance.js
+++ inheritance.js
@@ -17,7 +17,23 @@ getArea(){
 }
 //extends Rectangle 의 클래스를 만들고 싶다면 Shape 를 연장해서 사용하는 extends
 class Rectangle extends Shape {}
-class Triangle extends Shape{}
+class Triangle extends Shape{
+  draw(){
+    super.draw(); //shape에 정의된 함수가 호출되지 않아서 같이 쓰려면 사용
+    console.log('☹︎');
+  }
+
+  getArea(){
+    return(this.width * this.height)/2;
+  }
+}
 
 const rectangle = new Rectangle(20,20,'blue');
+
 rectangle.draw(); //?drawing blue color ! 
+console.log(rectangle.getArea()); //?400
+
+const triangle = new Triangle(20,20,'red');
+
+triangle.draw();//? drawing red color!
+console.log(triangle.getArea());  //?200
